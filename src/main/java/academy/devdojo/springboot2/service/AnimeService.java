8fa2bc004094db2fda9850291792613ca7332f85
Felix-Linux <rodrigofelixferreira@hotmail.com>
2024-02-12 17:57:15 -0300

    added video 07 - Refactored Project

---
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -0,0 +1,14 @@
+package academy.devdojo.springboot2.service;
+
+import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AnimeService {
+    public List<Anime> listAll() {
+        return List.of(new Anime(1L, "Boku no Hero"), new Anime(2L, "Beserk"));
+
+    }
+}
