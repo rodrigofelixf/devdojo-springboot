c77d3ce37fd8b6921b90f9f5d7cb4cc36d344c51
Felix-Linux <rodrigofelixferreira@hotmail.com>
2024-02-14 15:47:21 -0300

    added video 17, 18, custom exception and global handler exceptions

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.mapper.AnimeMapper;
 import academy.devdojo.springboot2.repository.AnimeRepository;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
@@ -21,9 +22,13 @@ public class AnimeService {
         return animeRepository.findAll();
     }
 
+    public List<Anime> findByName(String name) {
+        return animeRepository.findByName(name);
+    }
+
     public Anime findByIdThrowBadRequestException(long id) {
         return animeRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+                .orElseThrow(() -> new BadRequestException("Anime not Found"));
     }
 
     public Anime save(AnimePostRequestBody animePostRequestBody) {
